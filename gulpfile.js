742b0ae785275b1a4310d494ad3a73d005834490
Vera <verkaevg@mail.ru>
2018-10-28 23:22:58 +0400

    Исправление замечаний

--- gulpfile.js
+++ gulpfile.js
@@ -24,39 +24,39 @@ gulp.task("css", function () {
     .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/css"));
-    //.pipe(server.stream());
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
 });
 
 gulp.task("images", function () {
- return gulp.src("source/img/**/*.{png,jpg,svg}")
- .pipe(imagemin([
- imagemin.optipng({optimizationLevel: 3}),
- imagemin.jpegtran({progressive: true}),
- imagemin.svgo()
- ]))
- .pipe(gulp.dest("source/img"));
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("html", function () {
- return gulp.src("source/*.html")
- .pipe(posthtml())
- .pipe(gulp.dest("build"));
+  return gulp.src("source/*.html")
+    .pipe(posthtml())
+    .pipe(gulp.dest("build"));
 });
 
 gulp.task("clean", function () {
- return del("build");
+  return del("build");
 });
 
 gulp.task("copy", function () {
- return gulp.src([
- "source/fonts/**/*.{woff,woff2}",
- "source/img/**",
- "source/js/**"
- ], {
- base: "source"
- })
- .pipe(gulp.dest("build"));
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**"
+  ], {
+  base: "source"
+  })
+  .pipe(gulp.dest("build"));
 });
 
 gulp.task("server", function () {
@@ -68,13 +68,13 @@ gulp.task("server", function () {
     ui: false
   });
 
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
+  gulp.watch("source/less/**/*.less", gulp.series("css", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
 gulp.task("refresh", function (done) {
- server.reload();
- done();
+  server.reload();
+  done();
 });
 
 gulp.task("build", gulp.series("clean", "copy", "css", "html"));
