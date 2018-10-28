3384917301ed40b430c8809f9b88ee54a2d9f96a
Vera <verkaevg@mail.ru>
2018-10-28 16:45:03 +0400

    Минификация и оптимизация изображений

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,14 @@
 var gulp = require("gulp");
 var less = require("gulp-less");
 var plumber = require("gulp-plumber");
+var rename = require("gulp-rename");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var gulp = require("gulp");
+var imagemin = require("gulp-imagemin");
 var server = require("browser-sync").create();
+
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -15,7 +20,20 @@ gulp.task("css", function () {
       autoprefixer()
     ]))
     .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("source/css"));
+    //.pipe(server.stream());
+});
+
+gulp.task("images", function () {
+ return gulp.src("source/img/**/*.{png,jpg,svg}")
+ .pipe(imagemin([
+ imagemin.optipng({optimizationLevel: 3}),
+ imagemin.jpegtran({progressive: true}),
+ imagemin.svgo()
+ ]))
+ .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("server", function () {
